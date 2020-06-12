690c67d3bd7966ce2ed332bfda8d7f44e7fbc706
Vasilis Athanasiou <billa8anasiou@gmail.com>
2020-06-12 14:00:30 +0300

    Added evaluate : Performs statistical analysis

--- create-dataset.py
+++ create-dataset.py
@@ -2,12 +2,12 @@ import numpy as np
 import cv2 as cv
 import os
 
-source_directory = 'datasets/sources/source_diverse/cloudy-images'
-templates_directory = 'datasets/templates/templates-diverse/cloudy'
+source_directory = 'datasets/sources/source_diverse/images'
+templates_directory = 'datasets/templates/templates-diverse/images'
 crop_width = 200
 
 # Open text file that will include image name and most left pixel
-file = open("dataset-diverse-info.txt", "w")
+file = open("datasets/templates/templates-diverse/dataset-diverse-loc.txt", "w")
 
 # Append each image path into a list
 source_paths = [os.path.join(source_directory, image_path) for image_path in os.listdir(source_directory)]
@@ -20,14 +20,13 @@ for image_path in source_paths:
     images.append(cv.imread(image_path))
 
 
-
 counter = 0
 for img in images:
     img_height, img_width, _ = img.shape
     for most_left_pixel in range(img_width - crop_width):
         cropped_img = img[0:img_height, most_left_pixel:most_left_pixel + crop_width]
         print(cropped_img.shape)
-        cv.imwrite('/home/haistudent/PycharmProjects/diphaiopencv/templates-diverse/{}.png'.format(counter), cropped_img)
+        cv.imwrite('/home/haistudent/PycharmProjects/diphaiopencv/datasets/templates/templates-diverse/images/{}.png'.format(counter), cropped_img)
         file.write("{}\n".format(most_left_pixel))
         counter += 1
 
--- main.py
+++ main.py
@@ -4,24 +4,10 @@ import os
 from matplotlib import pyplot as plt
 import random
 
-# Set image directory
-images_directory = 'datasets/sources/source_diverse/cloudy'
-templates_directory = 'datasets/templates/test-temp'
-match_pos_path = 'testdatasetdata.txt'
-
-# Append each image path into a list
-source_paths = [os.path.join(images_directory, image_path) for image_path in os.listdir(images_directory)]
-templates_paths = [os.path.join(templates_directory, template_path) for template_path in os.listdir(templates_directory)]
-source_paths.sort()
-templates_paths.sort(key=lambda name: int(os.path.splitext(os.path.basename(name))[0]))
-
-for elem in templates_paths:
-    print(elem)
 
 # ---------------------------------------------Image Processing--------------------------------------------------------- #
 
-
-def process_image(src_img, resize):
+def process_image(src_img, resize=0.5):
     # Resize image
     res_img = cv.resize(src_img, None, fx=resize, fy=resize, interpolation=cv.INTER_CUBIC)
 
@@ -32,8 +18,6 @@ def process_image(src_img, resize):
     gray_image = cv.blur(gray_image, (5, 5), gray_image)
 
     # Binarize image
-
-    # bin_img = cv.threshold(gray_image, 120, 255, cv.THRESH_BINARY)
     (thresh, bin_img) = cv.threshold(gray_image, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
 
     # Erode binary image
@@ -50,7 +34,7 @@ def process_image(src_img, resize):
 
 # ------------------------------------------ Matching Algorithm -------------------------------------------------------- #
 
-def find_pixel_dx(sat_img, temp_img):  # (satellite / source image, template image)
+def find_pixel_dx(sat_img, temp_img, resize_value=0.5):  # (satellite / source image, template image)
     # Store the image array shapes
     sat_height, sat_width, _ = sat_img.shape
     temp_height, temp_width, _ = temp_img.shape
@@ -59,7 +43,7 @@ def find_pixel_dx(sat_img, temp_img):  # (satellite / source image, template ima
     max_matrix = (np.zeros(temp_img.shape), 0)  # Will compare with comparison results, to find the greatest match
 
     processed_template = process_image(temp_img, resize_value)
-    cv.imshow('Processed Template', processed_template)
+    # cv.imshow('Processed Template', processed_template)
     # Compare every possible roi with the template by performing logical operations and find the greatest match
     for most_left_pixel in range(int(sat_width - temp_width)):  # Search the whole image
         # print("MLP : {} | TEMPWIDTH : {} | SUM : {}".format(most_left_pixel, temp_width, most_left_pixel + temp_width))
@@ -68,7 +52,8 @@ def find_pixel_dx(sat_img, temp_img):  # (satellite / source image, template ima
         processed_roi = process_image(roi, resize_value)
 
         and_result = np.logical_and(processed_roi, processed_template)  # Perform AND on each roi and template pixel (1)
-        xnor_result = np.invert(np.logical_xor(processed_roi, processed_template))  # Perform XNOR on each roi and template pixel (2)
+        xnor_result = np.invert(
+            np.logical_xor(processed_roi, processed_template))  # Perform XNOR on each roi and template pixel (2)
         comp_matrix.append((np.array(np.logical_or(and_result, xnor_result), dtype=int), most_left_pixel))  # (1) OR (2)
 
         comp_sum = int(np.sum(comp_matrix[most_left_pixel][0]))  # Sum the comparison matrix
@@ -76,7 +61,7 @@ def find_pixel_dx(sat_img, temp_img):  # (satellite / source image, template ima
 
         if comp_sum > max_sum:  # Find max comparison matrix
             max_matrix = np.copy(comp_matrix[most_left_pixel][0]), most_left_pixel  # Store matrix and most left pixel
-            cv.imshow('Processed ROI', processed_roi)
+            # cv.imshow('Processed ROI', processed_roi)
 
     return max_matrix  # Return the result
 
@@ -84,6 +69,58 @@ def find_pixel_dx(sat_img, temp_img):  # (satellite / source image, template ima
 # ---------------------------------------------------------------------------------------------------------------------- #
 
 # ------------------------------------------ Statistical Analysis ------------------------------------------------------ #
+
+def evaluate(src, temp, actual_match, resize_value=0.5):
+
+    error = float(0)  # Error for the whole set
+    error_img = []  # Error for each image Sum(error for every template / number of templates)
+    num_of_images = len(src)
+
+    counter = 0  # Keeps track of each iterations
+
+    for img in src:  # For every image
+        img_height, img_width, _ = img.shape
+        template_height, template_width, _ = temp[counter].shape
+
+        print("Source {}x{} , Template {}x{}".format(img_height, img_width, template_height, template_width))
+
+        templates_per_image = 1  # img_width - template_width  # This is the num of times the src will be scanned
+
+        is_middle = False  # Is used to print when we are in the middle of one image
+        error_temp_img = 0  # Error for one template. How displaced the sensed frame is from the actual one
+
+        for i in range(templates_per_image):
+            if i > int(templates_per_image / 2) and not is_middle:
+                print("Reached the middle of the image")
+                is_middle = True
+            matched_matrix_and_location = find_pixel_dx(img, temp[counter])
+            error_temp_img += float(
+                np.abs(matched_matrix_and_location[1] - int(actual_match[counter])) / img_width * resize_value)
+            counter += 1
+        error_img.append(error_temp_img / templates_per_image)
+        print("Error for image {} : {}".format(int(counter / templates_per_image), error_temp_img / templates_per_image))
+
+
+    error = (sum(error_img) / num_of_images) * 100
+
+    print("There is {}% error.".format(error))
+
+# ---------------------------------------------------------------------------------------------------------------------- #
+
+
+# Set image directory
+images_directory = 'datasets/sources/source_diverse/cloudy-images'
+templates_directory = 'datasets/templates/test-temp'
+match_pos_path = 'testdatasetdata.txt'
+
+# Append each image path into a list
+source_paths = [os.path.join(images_directory, image_path) for image_path in os.listdir(images_directory)]
+templates_paths = [os.path.join(templates_directory, template_path) for template_path in os.listdir(templates_directory)]
+source_paths.sort()
+templates_paths.sort(key=lambda name: int(os.path.splitext(os.path.basename(name))[0]))
+
+for elem in templates_paths:
+    print(elem)
 
 # Read the images
 source_images = []
@@ -97,46 +134,8 @@ for template_path in templates_paths:
 match_pos_file = open(match_pos_path, 'r')
 actual_match_position = match_pos_file.readlines()
 
-error = float(0)  # Error for each template, error for each image and error for the whole set
-error_img = []
+evaluate(source_images, templates, actual_match_position)
 
-counter = 0
-
-resize_value = 0.5
-
-for img in source_images:
-    img_height, img_width, _ = img.shape
-    template_height, template_width, _ = templates[counter].shape
-
-    print("Source {}x{} , Template {}x{}".format(img_height, img_width, template_height, template_width))
-
-    templates_per_image = 1  # img_width - template_width
-
-    error_temp_img = 0
-    for i in range(templates_per_image):
-
-        matched_matrix_and_location = find_pixel_dx(img, templates[counter])
-        # print("Sensed first pixel - actual first pixel : {}".format(
-        #     np.abs(matched_matrix_and_location[1] - int(actual_pixel_position[counter]))))
-        error_temp_img += float(np.abs(matched_matrix_and_location[1] - int(actual_match_position[counter])) / img_width * resize_value)
-        # print("Error for image {} and template {} : {:3f}".format(int(counter / templates_per_image), counter % templates_per_image, error_temp_img))
-        counter += 1
-    error_img.append(error_temp_img / templates_per_image)
-    print("Error for image {} : {}".format(int(counter / templates_per_image), error_temp_img / templates_per_image))
-    cv.imshow('Original Image', img)
-
-    #plt.imshow(matched_matrix_and_location[0])
-    #plt.show()
-
-    while True:
-        if cv.waitKey(0) == 27:
-            cv.destroyAllWindows()
-            plt.close()
-            break
-
-error = (sum(error_img) / len(source_images)) * 100
-
-print("There is {}% error.".format(error))
 # ---------------------------------------------------------------------------------------------------------------------- #
 
 
@@ -146,3 +145,8 @@ print("There is {}% error.".format(error))
 #                                                                            match_matrix_and_location[1]))
 
 # Close the window when the user presses the ESC key
+# while True:
+#     if cv.waitKey(0) == 27:
+#         cv.destroyAllWindows()
+#         plt.close()
+#         break
