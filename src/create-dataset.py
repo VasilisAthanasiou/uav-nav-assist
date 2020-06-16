f632f3686d54ce48b9c9efa6a254d88c4cbc581b
Vasilis Athanasiou <billa8anasiou@gmail.com>
2020-06-16 13:46:10 +0300

    main.py : Can now be used to experiment on various datasets

--- src/create-dataset.py
+++ src/create-dataset.py
@@ -5,13 +5,13 @@
 import cv2 as cv
 import os
 
-source_directory = '../datasets/sources/source-less-features/source'
-templates_directory = '../datasets/templates/templates-less-features/images'
+source_directory = '../datasets/sources/source-diverse/source'
+templates_directory = '../datasets/templates/templates-diverse/images'
 crop_height = 200
 crop_width = 200
 
 # Open text file that will include image name and most left pixel
-file = open("../datasets/templates/templates-less-features/less-features-loc.txt", "w")
+file = open("../datasets/templates/templates-diverse/dataset-diverse-loc.txt", "w")
 
 # Append each image path into a list
 source_paths = [os.path.join(source_directory, image_path) for image_path in os.listdir(source_directory)]
--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
 import cv2 as cv
 import numpy as np
 import os
-from matplotlib import pyplot as plt
-import random
+from scipy import ndimage
+
 
 # ---------------------------------------------Image Processing--------------------------------------------------------- #
 
-def process_image(src_img, resize=0.5):
+def process_image(src_img, resize=0.5, rotate=False, rot_deg=5):
 
     # # Resize image
     # res_img = cv.resize(src_img, None, fx=resize, fy=resize, interpolation=cv.INTER_CUBIC)
@@ -27,8 +27,13 @@ def process_image(src_img, resize=0.5):
     # # Dilate binary image
     # bin_img = cv.morphologyEx(bin_img, cv.MORPH_CLOSE, kernel)
 
-    return gray_image
+    processed_img = gray_image
 
+    # Rotate the image
+    if rotate:
+        processed_img = ndimage.rotate(gray_image, rot_deg)
+
+    return processed_img
 
 # ---------------------------------------------------------------------------------------------------------------------- #
 
@@ -42,19 +47,20 @@ def find_target(src, temp):
     res = cv.matchTemplate(src, temp, cv.TM_CCOEFF)
     min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
 
-    return max_loc # Return top left position
+    return max_loc  # Return top left position
 
 # ---------------------------------------------------------------------------------------------------------------------- #
 
 # ------------------------------------------ Statistical Analysis ------------------------------------------------------ #
 
-def evaluate(src, temp, actual_match, n_templates, resize_value=0.5):
+def evaluate(src, temp, actual_match, n_templates, resize_value=0.5, rotation=5):
 
     error = float(0)  # Error for the whole set
     error_img = []  # Error for each image Sum(error for every template / number of templates)
     num_of_images = len(src)
 
     counter = 0  # Keeps track of each iterations
+    result = ''
 
     for img in src:  # For every image
         img_height, img_width, _ = img.shape
@@ -66,32 +72,39 @@ def evaluate(src, temp, actual_match, n_templates, resize_value=0.5):
 
         templates_per_image = n_templates  # This is the num of times the src will be scanned
 
-        is_middle = False  # Is used to print when we are in the middle of evaluating
         error_temp_img = 0  # Error for one template. How displaced the sensed frame is from the actual one
 
         for i in range(templates_per_image):
             # Store both actual and sensed x and y
             actual_x, actual_y, _ = actual_match[i].split(',')
-            sensed_x, sensed_y = find_target(processed_img, process_image(temp[counter]))
+            sensed_x, sensed_y = find_target(processed_img, process_image(temp[counter], rotate=True, rot_deg=rotation))
+
+            # DEBUG
+            # cv.imshow('Template', process_image(temp[counter], rotate=True, rot_deg=rotation))
+            # while True:
+            #     if cv.waitKey(1) == 27:
+            #         cv.destroyAllWindows()
+            #         break
 
             # Error for one template is the added absolute differences between x and y divided the number of pixels
             error_temp_img += np.abs(int(sensed_x) - int(actual_x)) + np.abs(int(sensed_y) - int(actual_y))
             counter += 1
             print("For template {} : Sensed X : {} , Actual X : {} , Sensed Y : {} , Actual Y : {} , ERROR : {}".format(i, sensed_x, actual_x, sensed_y, actual_y, np.abs(int(sensed_x) - int(actual_x)) + np.abs(int(sensed_y) - int(actual_y))))
 
+
         error_img.append(error_temp_img / templates_per_image)  # Error for a whole image tested with multiple templates
-        print("Mean error for image {} : {}px".format(int(counter / templates_per_image), error_temp_img / templates_per_image))
+        result += ("Mean error for image {} : {}px\n".format(int(counter / templates_per_image), error_temp_img / templates_per_image))
 
     # Error for all images
-    error = sum(error_img)
+    error = sum(error_img)/num_of_images
 
-    print("There is {} pixel mean error.".format(error))
-
+    result += ("There is {} pixel mean error.\n\n".format(error))
+    return result
 # -------------------------------------------------Main----------------------------------------------------------------- #
 
 
 # Set image directory
-images_directory = '../datasets/sources/source-diverse/blurred-cloudy'
+images_directory = '../datasets/sources/source-diverse/source'
 templates_directory = '../datasets/templates/templates-diverse/images'
 match_pos_path = '../datasets/templates/templates-diverse/dataset-diverse-loc.txt'
 
@@ -100,6 +113,7 @@ source_paths = [os.path.join(images_directory, image_path) for image_path in os.
 templates_paths = [os.path.join(templates_directory, template_path) for template_path in os.listdir(templates_directory)]
 source_paths.sort()
 templates_paths.sort(key=lambda name: int(os.path.splitext(os.path.basename(name))[0]))
+
 
 # Print all paths to make sure everything is ok
 for elem in templates_paths:
@@ -120,6 +134,9 @@ match_pos_file = open(match_pos_path, 'r')
 actual_match_position = match_pos_file.readlines()
 
 # Evaluate the matching method. The method is hardcoded into the evaluation. This should be changed
-evaluate(source_images, templates, actual_match_position, 16)
+result_text = evaluate(source_images, templates, actual_match_position, 16, rotation=2)
 
+# Write the experiment results on a text file
+file = open("experiment-results.txt", "a")
+file.write("-------------- Results using 2deg rotation on source images --------------\n{}".format(result_text))
 # ---------------------------------------------------------------------------------------------------------------------- #
